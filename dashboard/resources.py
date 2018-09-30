d359cadea7401bd04b6792725f0921b766e816c9
lastranikos <nicolas.san@gmail.com>
2018-09-30 03:49:24 -0300

    authorization, permissions, and access control lists (ACLs)

---
+++ dashboard/resources.py
@@ -0,0 +1,12 @@
+from pyramid.security import Allow, Everyone
+
+
+class Root(object):
+    __acl__ = [(Allow, Everyone, 'view'),
+               (Allow, 'group:editors', 'edit'),
+               (Allow, 'group:viewers', 'edit'),
+               (Allow, 'group:administrators', 'edit'),
+               (Allow, 'group:users', 'edit')]
+
+    def __init__(self, request):
+        pass
